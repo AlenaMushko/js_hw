53ef622cfe8803fe401a19bbd3a57c61ce23f55c
AlenaMushko <evstafienko_a@ukr.net>
2023-05-11 16:47:58 +0300

    hw_4

--- hw_4.js
+++ hw_4.js
@@ -1,24 +1,23 @@
-// ВСЕ ЗРОБИТИ СТРІЛОЧНИМИ ФУНКЦІЯМИ!
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-areaRectangle =(a,b) =>{
+function areaRectangle(a,b){
     return a*b;
 }
 console.log('areaRectangle',areaRectangle(2, 5));
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
- doublePi = ()=>{
+function doublePi(){
     return 2*3.14
 }
-areaCircle=(r)=>{
+function areaCircle(r){
     return doublePi()*r;
 }
 console.log('areaCircle', areaCircle(1))
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
- areaCylinder=(r, h)=>{
+function areaCylinder(r, h){
     return doublePi()*r*h;
 }
 console.log('areaCylinder', areaCylinder(1, 2))
 // - створити функцію яка приймає масив та виводить кожен його елемент
- arrElements=(arr)=> {
+function arrElements(arr) {
     for (const arrElement of arr) {
         console.log(arrElement);
     }
@@ -27,12 +26,12 @@ const numbers= [1, 2, 3, 4, 5];
 console.log('arrElements')
 arrElements(numbers);
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
-text=(teg, info)=>{
+function text(teg, info){
   document.write(`<div><${teg}>${info}</${teg}></div>`)
 }
 text('h4', 'створити функцію яка створює параграф з текстом. Текст задати через аргумент')
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-list=(text)=>{
+function  list(text){
     document.write('<ul>')
     for (let i = 0; i < 3; i++) {
         document.write(`<li><p>${text}</p></li>`);
@@ -43,7 +42,7 @@ list('створити функцію яка створює ul з трьома �
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
 // Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 document.write('<hr>')
-listWithUnknownLi =(a, text)=>{
+function  listWithUnknownLi(a, text){
     for (let i = 0; i < a; i++) {
         document.write(`<li><p>${text}</p></li>`);
     }
@@ -52,7 +51,7 @@ listWithUnknownLi =(a, text)=>{
 listWithUnknownLi( 3,'створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий')
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 document.write('<hr>')
-fooArr=(arr)=>{
+function fooArr(arr){
     document.write('<ul>')
     for (let arrElement of arr) {
         document.write(`<li><p>${arrElement}</p></li>`);
@@ -69,7 +68,7 @@ let usersWithId = [
         {id: 3, name: 'kolya', age: 29},
         {id: 4, name: 'olya', age: 28}
     ];
-arrObj=(users)=>{
+function arrObj(users){
     for (let user of users) {
        document.write(`<di>
             <h2><spam style="color: cadetblue">${user.id}) </spam>${user.name}</h2>
@@ -79,7 +78,7 @@ arrObj=(users)=>{
 }
 arrObj(usersWithId)
 // - створити функцію яка повертає найменьше число з масиву
-numbersOfArr =(arr)=>{
+function numbersOfArr (arr){
     let numbersArr = [];
     for (const numberOfArr of arr) {
         if (typeof numberOfArr === 'number') {
@@ -88,7 +87,7 @@ numbersOfArr =(arr)=>{
     }
     return numbersArr;
 }
-minNumber=(arr)=>{
+function minNumber(arr){
     const numbersArr = numbersOfArr (arr);
     return Math.min(...numbersArr)
 }
@@ -96,7 +95,7 @@ const numbersArr = [2,5,1,66,24, '8',222, -534];
 console.log(`min number of numbers ${numbersArr} =>`, minNumber(numbersArr))
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його.
 // Приклад sum([1,2,10]) //->13
-sum=(arr)=>{
+function sum(arr){
     const numbersArr = numbersOfArr (arr);
     return numbersArr.reduce((acc, element) => {return acc+element },0)
 }
@@ -104,14 +103,14 @@ let arrForSum = [1,2,10]
 console.log(`sum of ${arrForSum} =`, sum(arrForSum))
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
-swap=(arr,index1,index2)=>{
+function swap(arr,index1,index2){
    [ arr[index1], arr[index2]] = [arr[index2], arr[index1]]
   return arr;
 }
 console.log('swap 0,1 elements of  arr [11,22,33,44]', swap([11,22,33,44],0,1))
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
-exchange=(sumUAH,currencyValues,exchangeCurrency)=>{
+function exchange(sumUAH,currencyValues,exchangeCurrency){
     for (let currencyValue of currencyValues) {
         if(currencyValue.currency === exchangeCurrency){
             return sumUAH / currencyValue.value;
