f391ff235c0770aa7c7e4b0b64f8d667ba52016b
AlenaMushko <evstafienko_a@ukr.net>
2023-06-01 11:54:14 +0300

    hw_10

--- hw_10.js
+++ hw_10.js
@@ -27,7 +27,7 @@ btnAge.innerText = 'age verification';
 btnAge.style.marginLeft = '16px';
 
 let p = document.createElement('p');
-form.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     let ageUser = parseInt(input.value);
 
@@ -35,7 +35,7 @@ form.addEventListener('click', (e) => {
     if (ageUser) {
         ageUser < 18 ? p.innerText = 'You are under 18 years old.' : p.innerText = 'You are 18 years old or older.';
     }
-    input.value = '';
+    e.currentTarget.reset();
     form.appendChild(p);
 })
 form.append(input, btnAge)
@@ -59,7 +59,7 @@ let btnSubmit = document.createElement('button');
 btnSubmit.innerText = 'Submit';
 
 const divObj = document.createElement('div');
-btnSubmit.addEventListener('click', (e) => {
+formBig.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const name = inputName.value;
@@ -75,7 +75,7 @@ btnSubmit.addEventListener('click', (e) => {
         divObj.innerText = JSON.stringify(user);
     }
 
-    formBig.reset();
+    e.currentTarget.reset();
 });
 
 formBig.append(inputName, inputSurname, inputAge, btnSubmit, divObj);
@@ -121,7 +121,7 @@ const sessionList = document.createElement('ul');
 for (let i = 0; i < sessions.length; i++) {
     let sessionItem = document.createElement('li');
     sessionItem.style.display = 'flex';
-    sessionItem.style.alignItems='center';
+    sessionItem.style.alignItems = 'center';
     sessionItem.style.gap = '16px';
 
     let numberOfSession = document.createElement('span');
@@ -147,29 +147,169 @@ for (let i = 0; i < sessions.length; i++) {
 document.body.append(sessionList)
 document.write(`<hr>`)
 // ===>
-//     зробити масив на 100 об'єктів та дві кнопки prev next
-// при завантажені сторінки з'являються перші 10 об'єктів.
+// зробити масив на 100 об'єктів та дві кнопки prev next
+let arrBox = document.createElement('div');
+
+let btnPrev = document.createElement('button');
+btnPrev.innerText = 'Prev 10 items';
+btnPrev.style.marginRight = '20px';
+
+let btnNext = document.createElement('button');
+btnNext.innerText = 'Next 10 items';
+let arr = [];
+let arrList = document.createElement('ul');
+//створюємо масив із 100 елементів
+for (let i = 0; i < 100; i++) {
+    arr.push(`це обєкт № ${i + 1}`)
+}
+;
+// розбиваємо масив із 100 елементів на масив із обєктами по 10 елементів
+let firstId = 0;
+let lastId = 10;
+
 //     При натисканні next виводяться настпні 10 об'єктів
+let errorText = document.createElement('p');
+errorText.style.color = 'red';
+btnNext.addEventListener('click', () => {
+    errorText.innerText = '';
+    if (lastId < arr.length) {
+        btnPrev.disabled = false;
+        firstId += 10;
+        lastId += 10;
+        showItems();
+        return;
+    } else {
+        btnNext.disabled = true;
+        errorText.innerText = 'End of Items';
+    }
+});
 // При натисканні prev виводяться попередні 10 об'єктів
-document.write(`<hr>`)
-// ===>
-// - Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав
-// елемент з id="text".
-document.write(`<hr>`)
-// ===>
-//     - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та
-//     перевірити вік чи
-//     меньше він ніж 18, та повідомити про це користувача
-document.write(`<hr>`)
+btnPrev.addEventListener('click', () => {
+    errorText.innerText = '';
+    if (firstId > 0) {
+        btnNext.disabled = false;
+        firstId -= 10;
+        lastId -= 10;
+        showItems();
+        return;
+    } else {
+        btnPrev.disabled = true;
+        errorText.innerText = 'End of Items';
+    }
+});
+const showItems = () => {
+    arrList.innerHTML = '';
+
+    const tenItems = arr.slice(firstId, lastId);
+// при завантажені сторінки з'являються перші 10 об'єктів.
+    for (let i = 0; i < tenItems.length; i++) {
+        let arrItem = document.createElement('li');
+        arrItem.innerText = tenItems[i];
+        arrItem.style.fontSize = '22px';
+        arrList.appendChild(arrItem);
+    }
+};
+showItems();
+arrBox.append(btnPrev, btnNext, arrList, errorText)
+document.body.appendChild(arrBox);
+document.write(`<hr>`);
 // ===>
 // *** Створити 3 інпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
-//     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
-// (Додатковачастина для завдання)
+let newDiv = document.createElement('div')
+
+let newForm = document.createElement('form');
+newForm.style.display = 'flex';
+newForm.style.gap = '24px';
+
+let newInputLine = document.createElement('input');
+newInputLine.placeholder = 'кількість рядків';
+
+let newInputNumOfCell = document.createElement('input');
+newInputNumOfCell.placeholder = 'кількість ячеєк';
+
+let newInputCell = document.createElement('input');
+newInputCell.placeholder = 'вмиіст ячеєк';
+
+let newBtn = document.createElement('button');
+newBtn.innerText = 'формуємо табличку';
+
+newForm.addEventListener('submit', onSubmitNewForm);
+
+// При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
+function onSubmitNewForm(e) {
+    e.preventDefault();
+    let line = parseInt(newInputLine.value);
+    let numOfCell = parseInt(newInputNumOfCell.value);
+    let cell = newInputCell.value;
+
+    if (isNaN(line) || isNaN(numOfCell)) {
+        let errorText = document.createElement('p');
+        errorText.style.color = 'red';
+        errorText.innerText = 'введіть дані у всі поля';
+        document.body.append(errorText);
+    }
+    buildTable(line, numOfCell, cell);
+    e.currentTarget.reset();
+}
+
+function buildTable(line, cell, text) {
+    const table = document.createElement('table');
+    table.style.borderCollapse = 'collapse';
+
+    for (let i = 0; i < line; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < cell; j++) {
+            const cell = document.createElement('td');
+            cell.innerText = text;
+            cell.style.border = '1px solid black';
+            cell.style.padding = '8px'
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+    newDiv.append(table);
+}
+
+newForm.append(newInputLine, newInputNumOfCell, newInputCell, newBtn);
+newDiv.append(newForm);
+document.body.appendChild(newDiv);
 document.write(`<hr>`)
 // ===>
 // *** (подібне було вище, але...будьте уважні в другій частині) створити сторінку з довільним блоком, в середині якого є значення "100грн"
+let newBlock = document.createElement('div');
+newBlock.id = 'text';
+newBlock.style.width = '100px';
+newBlock.style.height = '35px';
+newBlock.style.background = 'yellow';
+
+let counterMoney = JSON.parse(localStorage.getItem('counterMoney')) || 100;
+localStorage.setItem('counterMoney', JSON.stringify(counterMoney))
+
+let money = document.createElement('p')
+money.innerText = `${counterMoney}UAN`;
+money.style.color = 'blue';
+money.style.fontSize = '30px';
+money.style.textAlign = 'center';
 // при перезавантаженні сторінки до значаення додається по 10грн, але !!!
-//     зміна ціни відбувається тільки на перезавантаження, які відбулись пізніше ніж 10 секунд після попереднього.
+// час останнього оновлення з localStorage
+const lastUpdateTime = localStorage.getItem('lastUpdateTime') || '';
+const currentTime = new Date().getTime();
+localStorage.setItem('lastUpdateTime', currentTime.toString());
+
+// Перевірити, чи пройшло 10 секунд з останнього оновлення
+const isPassedTenS = currentTime - parseInt(lastUpdateTime) >= 10000;
+//  зміна ціни відбувається тільки на перезавантаження, які відбулись пізніше ніж 10 секунд після попереднього.
 //     При перезавантаженні, яке відбулось раніше ніж минуло 10 секунд - нічого не відбувається
+window.onload = function () {
+    if (isPassedTenS) {
+        counterMoney += 10;
+        money.innerText = `${counterMoney}UAN`;
+        localStorage.setItem('counterMoney', JSON.stringify(counterMoney));
+    };
+    localStorage.setItem('lastUpdateTime', currentTime.toString());
+};
+
+newBlock.append(money);
+document.body.append(newBlock);
 
 
